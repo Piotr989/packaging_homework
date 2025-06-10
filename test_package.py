bf02a3206a086526f7f32103cf816235a1452aeb
Piotr989 <piotrdada613@gmail.com>
2025-06-10 17:37:49 +0200

    Added pyproject.py, setup.cfg, modified test_package.py to print results

--- test_package.py
+++ test_package.py
@@ -3,9 +3,15 @@ import numpy as np
 
 def test_some_calculation_function():
     result = some_calculation_function()
+    print("fun1:\n", result)
     assert np.all(result == 0)
     assert result.shape == (4, 7)
 
-
 def test_some_calculation_function2():
-    assert some_calculation_function2() == 18
+    result = some_calculation_function2()
+    print("fun2:", result)
+    assert result == 18
+
+if __name__ == "__main__":
+    test_some_calculation_function()
+    test_some_calculation_function2()
